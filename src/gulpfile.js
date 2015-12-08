6027cc1fa7ec27990b7b25465da563f32cfe5f3e
sirmosca <mosca125@gmail.com>
2015-12-07 21:03:46 -0500

    add uglify

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('scripts', function() {
 	return gulp.src('client/js/draftbuddy-app.js')
 		.pipe(concat('main.js'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
 		.pipe(gulp.dest('build/js'));
 });
 
