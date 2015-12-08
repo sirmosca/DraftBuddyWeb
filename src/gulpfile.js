4c963ee611c05d83704f4886a07334856fc78055
sirmosca <mosca125@gmail.com>
2015-12-07 21:36:12 -0500

    delete build folder prior to running build

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -2,8 +2,16 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var del = require('del');
+var vinylPaths = require('vinyl-paths');
 
-gulp.task('scripts', function() {
+gulp.task('clean', function() {
+	return gulp.src('build/js')
+		.pipe(vinylPaths(del))
+		.pipe(gulp.dest('build'));
+});
+
+gulp.task('scripts', ['clean'], function() {
 	return gulp.src('client/js/draftbuddy-app.js')
 		.pipe(concat('main.js'))
 		.pipe(rename({suffix: '.min'}))
@@ -11,4 +19,4 @@ gulp.task('scripts', function() {
 		.pipe(gulp.dest('build/js'));
 });
 
-gulp.task('default', ['scripts']);
+gulp.task('default', ['clean', 'scripts']);
