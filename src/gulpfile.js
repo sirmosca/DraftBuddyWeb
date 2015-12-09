a3dce6526160e7bab8fdc2dba05d589ba50b7855
sirmosca <mosca125@gmail.com>
2015-12-08 20:55:07 -0500

    added copying of libs

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -7,11 +7,14 @@ var vinylPaths = require('vinyl-paths');
 var Server = require('karma').Server;
 var runSequence = require('run-sequence');
 var replace = require('gulp-token-replace');
+var destination = "build/";
+var destinationJs = destination + '/js/'
+
 
 gulp.task('clean', function() {
 	return gulp.src('build')
 		.pipe(vinylPaths(del))
-		.pipe(gulp.dest('build'));
+		.pipe(gulp.dest(destination));
 });
 
 gulp.task('scripts', function() {
@@ -21,7 +24,7 @@ gulp.task('scripts', function() {
 		.pipe(rename({suffix: '.min'}))
 		.pipe(replace({tokens:config}))
 //		.pipe(uglify())
-		.pipe(gulp.dest('build/js'));
+		.pipe(gulp.dest(destinationJs));
 });
 
 gulp.task('test', function(done) {
@@ -33,12 +36,19 @@ gulp.task('test', function(done) {
 
 gulp.task('html', function() {
 	gulp.src('client/index.html')
-		.pipe(gulp.dest('build'));
+		.pipe(gulp.dest(destination));
 });
 
 gulp.task('css', function() {
 	gulp.src('client/style.css')
-		.pipe(gulp.dest('build'));
+		.pipe(gulp.dest(destination));
+});
+
+gulp.task('libs', function() {
+	gulp.src('client/js/jquery-2.1.1.js')
+		.pipe(gulp.dest(destinationJs));
+	gulp.src('client/js/ui-bootstrap-tpls-0.13.0.js')
+		.pipe(gulp.dest(destinationJs));
 });
 
 gulp.task('build', function(cb) {
