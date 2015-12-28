6c410b5e47232fcc3b0753520d81465cd4e50279
sirmosca <mosca125@gmail.com>
2015-12-27 21:18:58 -0500

    include teamService

--- src/client/js/app/app.js
+++ src/client/js/app/app.js
@@ -1,3 +1,4 @@
 ﻿var app = angular.module('draftbuddy', ['ui.bootstrap']);
 app.controller('playerCtrl', PlayerController);
 app.factory('playerService', PlayerService);
+app.factory('teamService', TeamService);
